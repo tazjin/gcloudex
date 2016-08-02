bb4856550e5ee1164a07472cc46cdfe69f9b4810
Vincent Ambo <tazjin@gmail.com>
2016-08-02 18:59:50 +0200

    [lib/GCloudex] Get project ID from Goth.Config
    Instead of parsing the Goth application configuration, retrieve
    the project ID from the Goth.Config process.
    
    This does not currently work with how get_project_id is called at
    compile time in the rest of the project.
    
    Please see issue #4 for more information.

--- lib/gcloudex.ex
+++ lib/gcloudex.ex
@@ -1,17 +1,16 @@
 defmodule GCloudex do
-
   @moduledoc """
   Set of wrappers for Google Cloud Platform's services API's.
   """
 
   @doc """
-  Returns the Google Project ID specified in the creds.json file in the configs.
+  Fetches the Google Project ID from Goth.
+  Goth retrieves this either through the configured credentials or from Google's
+  metadata service if the application is running on Google Cloud Platform.
   """
   @spec get_project_id :: binary
   def get_project_id do
-    :goth
-    |> Application.get_env(:json) 
-    |> Poison.decode! 
-    |> Map.get("project_id")
+    {:ok, project_id} = Goth.Config.get(:project_id)
+    project_id
   end
 end
